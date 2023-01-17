83d5448e0dfc1db6968778184605ff3b4d9b6bf2
ndawulajoe <ndawulajoseph255@gmail.com>
2023-01-17 18:57:55 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let sum = 0;
+for(i=0;i<1000;i++){
+    if(i%3===0|| i%5===0){
+        
+      let res=sum+=i
+        console.log(res)
+    }
+    
+}
+ console.log(sum)
+// let sum =0
+// const numbersbelow=[1000];
+// for (let num of numbersbelow){
+//     console.log(num)
+// }
